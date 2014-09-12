fc52e5f50b1e93d6f7dedd11385e1fa2a09658e6
vilmar23 <vilmar_mcb@hotmail.com>
2014-09-12 12:28:30 +0200

    cambio de clase

--- www/adstream.js
+++ www/adstream.js
@@ -10,14 +10,14 @@ var AdStream = function(src, successCallback, errorCallback, statusCallback) {
     this.statusCallback = statusCallback;
     this._duration = -1;
     this._position = -1;
-    exec(null, this.errorCallback, "AdStream", "create", []);
+    exec(null, this.errorCallback, "Adstream", "create", []);
 };
 
 /**
  * Start or resume playing audio file.
  */
 AdStream.prototype.muestraAdd = function() {
-    exec(null, null, "AdStream", "muestra", []);
+    exec(null, null, "Adstream", "muestra", []);
 };
 
 module.exports = AdStream;
